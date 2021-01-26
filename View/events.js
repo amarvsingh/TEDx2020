0dd75917de3888f679aedf22fab993911769703c
meetishah13 <meetishah13@gmail.com>
2021-01-26 18:32:29 +0530

    Events page layout

--- View/events.js
+++ View/events.js
@@ -1,1 +1,17 @@
 !function(){window;const e=document.documentElement;if(e.classList.remove("no-js"),e.classList.add("js"),document.body.classList.contains("has-animations")){(window.sr=ScrollReveal()).reveal(".reveal-on-scroll",{duration:600,distance:"20px",easing:"cubic-bezier(0.5, -0.01, 0, 1.005)",origin:"left",interval:100})}}();
+var text = document.getElementById('text');
+var newDom = '';
+var animationDelay = 6;
+
+for(let i = 0; i < text.innerText.length; i++)
+{
+    newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+}
+
+text.innerHTML = newDom;
+var length = text.children.length;
+
+for(let i = 0; i < length; i++)
+{
+    text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+}
